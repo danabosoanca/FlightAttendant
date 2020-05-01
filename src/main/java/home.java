bc07b004e70ae34907b1389885c9f8f33f57df60
Dana <dana_bosoanca@yahoo.com>
2020-05-01 21:24:26 +0300

    Java Swing-Maven-Home

---
+++ src/main/java/home.java
@@ -0,0 +1,69 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import java.awt.Color;
+
+public class home extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					home frame = new home();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public home() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 461, 370);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(Color.PINK);
+		contentPane.add(panel, BorderLayout.CENTER);
+		panel.setLayout(null);
+		
+		JButton btnNewButton_1 = new JButton("Register");
+		btnNewButton_1.setIcon(new ImageIcon(home.class.getResource("/img/register.png")));
+		btnNewButton_1.setFont(new Font("Tahoma", Font.BOLD, 16));
+		btnNewButton_1.setBounds(246, 170, 130, 25);
+		panel.add(btnNewButton_1);
+		
+		JLabel label = new JLabel("");
+		label.setHorizontalAlignment(SwingConstants.CENTER);
+		label.setIcon(new ImageIcon(home.class.getResource("/img/iconfinder_coronovirus_02_5826047.png")));
+		label.setBounds(118, 31, 160, 115);
+		panel.add(label);
+		
+		JButton btnNewButton = new JButton("Log in");
+		btnNewButton.setFont(new Font("Tahoma", Font.BOLD, 16));
+		btnNewButton.setIcon(new ImageIcon(home.class.getResource("/img/login.png")));
+		btnNewButton.setBounds(69, 170, 116, 25);
+		panel.add(btnNewButton);
+	}
+}
